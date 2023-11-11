4a0514856e13f3d27246df326496e72b747aa02e
Saurav Hathi <61316762+sauravhathi@users.noreply.github.com>
2023-11-11 10:41:02 +0530

    Update schedulerController.js

--- app/controllers/schedulerController.js
+++ app/controllers/schedulerController.js
@@ -4,7 +4,16 @@ const logger = require('../utils/logger');
 const cronSchedule = process.env.CRON_SCHEDULE || '0 0 * * *';
 
 // Clear expired OTPs every day at midnight by default
-cron.schedule(cronSchedule, async () => {
+// cron.schedule(cronSchedule, async () => {
+//     try {
+//         await otpController.clearExpiredOtps();
+//         logger.info('Cleared expired OTPs');
+//     } catch (error) {
+//         logger.error('Failed to clear expired OTPs', error.message);
+//     }
+// });
+
+const scheduledTask = cron.schedule(cronSchedule, async () => {
     try {
         await otpController.clearExpiredOtps();
         logger.info('Cleared expired OTPs');
@@ -12,3 +21,5 @@ cron.schedule(cronSchedule, async () => {
         logger.error('Failed to clear expired OTPs', error.message);
     }
 });
+
+module.exports = scheduledTask;
