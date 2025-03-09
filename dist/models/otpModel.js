b1a82df287570ab6d0ab82c97043e5c534220b41
Saurav Hathi <61316762+sauravhathi@users.noreply.github.com>
2025-03-10 01:14:35 +0530

    Committing dist/models/otpModel.js

---
+++ dist/models/otpModel.js
@@ -0,0 +1,46 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importStar(require("mongoose"));
+const expiryTime = parseInt(process.env.OTP_VALIDITY_PERIOD_MINUTES || '5') + 1;
+const otpSchema = new mongoose_1.Schema({
+    id: { type: String, required: true },
+    email: { type: String, required: true, index: true },
+    otp: { type: String, required: true },
+    attempts: { type: Number, default: 0 }
+}, { timestamps: true });
+otpSchema.index({ createdAt: 1 }, { expireAfterSeconds: expiryTime * 60 });
+const Otp = mongoose_1.default.model('Otp', otpSchema);
+exports.default = Otp;
