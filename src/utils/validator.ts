080a741f40f9e7cd20c4cbee334bf5d9bf22b18a
Saurav Hathi <61316762+sauravhathi@users.noreply.github.com>
2025-03-10 01:14:40 +0530

    Committing src/utils/validator.ts

---
+++ src/utils/validator.ts
@@ -0,0 +1,17 @@
+export const isValidEmail = (email: string): boolean => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const [localPart, domain] = email.split("@");
+    const allowedDomains = process.env.ALLOWED_DOMAINS
+        ? process.env.ALLOWED_DOMAINS.split(",")
+        : [];
+
+    const minLocalPartLength = 5;
+    const maxLocalPartLength = 64;
+
+    return (
+        (allowedDomains.length === 0 || allowedDomains.includes(domain)) &&
+        localPart.length >= minLocalPartLength &&
+        localPart.length <= maxLocalPartLength &&
+        emailRegex.test(email)
+    );
+};
