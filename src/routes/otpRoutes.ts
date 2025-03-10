d798543862c6a31cdd82147fce0fd81a1b593af0
Saurav Hathi <61316762+sauravhathi@users.noreply.github.com>
2025-03-10 21:58:40 +0530

    Committing src/routes/otpRoutes.ts

--- src/routes/otpRoutes.ts
+++ src/routes/otpRoutes.ts
@@ -31,7 +31,7 @@ router.post('/otp/generate', validateSpamMiddleware, async (req, res) => {
 router.post('/otp/verify', async (req, res) => {
   try {
     const { email, otp } = req.body;
-    await otpController.verifyOtp(email, otp);
+    await otpController.verifyOtp(email, otp?.toString());
 
     res.status(200).json({ message: 'OTP is verified' });
   } catch (error) {
